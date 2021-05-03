97c2913a1ffac7fa176417e01c6d6c0f8a3edd64
deepdong <donghyunmon@gmail.com>
2021-05-03 03:26:24 -0500

    check

--- nt_transfer/utils.py
+++ nt_transfer/utils.py
@@ -146,9 +146,9 @@ def get_masks_from_jax_params(params, nn_density_level, magnitude_base_bool = Tr
         raise ValueError("magnitude_base_bool and global_bool should be boolean variables")
     
     masks = []
-    sub_masks=[]
-    block_masks=[]
-    layer_masks=[]
+    #sub_masks=[]
+    #block_masks=[]
+    #layer_masks=[]
     
     if global_bool:
         weight_magnitudes_pooled = np.concatenate([ np.abs(layer_params[0].flatten()) for layer_params in params if len(layer_params) > 1])
@@ -220,14 +220,14 @@ def get_masks_from_jax_params(params, nn_density_level, magnitude_base_bool = Tr
                                     # in the case of random pruning: randomly shuffle the mask
                                     this_mask = random.shuffle(random.PRNGKey(0), this_mask )
 
-                            layer_masks.append(this_mask )
+                            masks.append(this_mask )
 
                         else:
                             print(len(params[layer_index][subNN][block][sub_layer]))
                             raise NotImplementedError
-                    block_masks.append(layer_masks)
-                sub_masks.append(block_masks)
-            masks.append(sub_masks)
+                    masks.append(masks)
+                masks.append(masks)
+            masks.append(masks)
 
     return masks
 
