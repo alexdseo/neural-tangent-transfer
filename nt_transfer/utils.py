2a2fcc15b71dfc3f32e4719825954cc2db8333f3
deepdong <donghyunmon@gmail.com>
2021-05-03 02:56:06 -0500

    test

--- nt_transfer/utils.py
+++ nt_transfer/utils.py
@@ -277,7 +277,6 @@ def get_sparse_params_filtered_by_masks(params, masks):
 
                             mask_this_layer = masks[layer_index][subNN][block][sub_layer]
 
-                            print(len(weights))
                             print(len(mask_this_layer))
                             # sparse weights gated by masks
                             sparse_weights = np.multiply(mask_this_layer, weights )
